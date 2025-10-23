5d68262c9a0135dcdaea81859502e833ffb3f87e
kylelovestoad <kylelovestoad+github@protonmail.com>
2025-10-23 16:30:50 -0400

    Fix typing issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import {useCallback, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route, Routes} from "react-router";
+import Schedule from "./schedule/Schedule.tsx";
 
 function App() {
     const [count, setCount] = useState(0)
@@ -24,32 +24,11 @@ function App() {
     }, [name, password, username]);
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <input onChange={e => setName(e.target.value)}/>
-                <input onChange={e => setUsername(e.target.value)}/>
-                <input onChange={e => setPassword(e.target.value)}/>
-                <button onClick={signup}/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <Routes>
+            <Route path="/login" element={(<p>TEST</p>)} />
+            <Route path="/signup" element={(<p/>)} />
+            <Route path="/calendar" element={(<Schedule />)}/>
+        </Routes>
     )
 }
 
