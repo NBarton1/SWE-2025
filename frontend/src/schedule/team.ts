da3a5cdeb1689bd52b82d71b53b75a28ab93b1ab
joe <josephfielding03@gmail.com>
2025-10-24 16:38:34 -0400

    removed jwt

--- frontend/src/schedule/team.ts
+++ frontend/src/schedule/team.ts
@@ -7,3 +7,13 @@ export interface Team {
     draw: number
     loss: number
 }
+
+export function getPCT(team: Team): number {
+    return (team.win + team.draw / 2) / (team.win + team.loss + team.draw);
+}
+
+export function formatPCT(team: Team) {
+    const pct = getPCT(team);
+
+    return isNaN(pct) ? "-" : pct.toFixed(3).replace(/^0/, "");
+}
