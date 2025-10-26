22ecfca1153c8a2d7d53f397fa4c969b5bcce29d
nmbar <nmbarton1@loyola.edu>
2025-10-26 01:13:08 -0400

    team controller tests

--- backend/src/test/kotlin/com/jknv/lum/controller/TeamControllerTest.kt
+++ backend/src/test/kotlin/com/jknv/lum/controller/TeamControllerTest.kt
@@ -1,110 +1,97 @@
 package com.jknv.lum.controller
 
+import com.jknv.lum.model.dto.*
 import com.jknv.lum.model.entity.Account
 import com.jknv.lum.model.entity.Coach
 import com.jknv.lum.model.entity.Team
+import com.jknv.lum.model.request.team.TeamCreateRequest
+import com.jknv.lum.model.type.InviteStatus
 import com.jknv.lum.model.type.Role
 import com.jknv.lum.services.CoachService
+import com.jknv.lum.services.TeamInviteService
 import com.jknv.lum.services.TeamService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.transaction.annotation.Transactional
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.HttpStatus
 import java.security.Principal
-import kotlin.test.assertTrue
 
-
-@SpringBootTest
-@Transactional
+@ExtendWith(MockKExtension::class)
 class TeamControllerTest {
-    @Autowired
-    lateinit var teamController: TeamController
+    var teamService: TeamService = mockk()
+    var teamInviteService: TeamInviteService = mockk()
+    var coachService: CoachService = mockk()
 
-    @Autowired
-    lateinit var teamService: TeamService
+    var teamController: TeamController = TeamController(teamService, teamInviteService, coachService)
 
-    @Autowired
-    lateinit var coachService: CoachService
+    var principal: Principal = Principal { "coach" }
 
+    lateinit var req: TeamCreateRequest
     lateinit var team: Team
+    lateinit var teamDTO: TeamDTO
 
     @BeforeEach
-    fun setup() {
-        team = teamService.createTeam(Team(name = "Home Team"))
+    fun setUp() {
+        req = TeamCreateRequest(name = "team")
+        team = req.toEntity()
+        teamDTO = team.toDTO()
     }
 
     @Test
-    @WithMockUser(roles = ["ADMIN"])
+    fun createTeamTest() {
+        every { teamService.createTeam(req) } returns teamDTO
+
+        val result = teamController.create(req)
+
+        assertEquals(HttpStatus.CREATED, result.statusCode)
+        assertEquals(teamDTO, result.body)
+        verify { teamService.createTeam(req) }
+    }
+
+    @Test
+    fun getAllTeamsTest() {
+        every { teamService.getTeams() } returns listOf(teamDTO)
+
+        val result = teamController.getAll()
+
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertEquals(listOf(teamDTO), result.body)
+        verify { teamService.getTeams() }
+    }
+
+    @Test
     fun addCoachTest() {
-        val account = Account(
-            name = "name",
-            username = "username",
-            password = "password",
-            role = Role.ADMIN,
-        )
+        val coach = Coach(account = Account(name = "coach", username = principal.name, password = "password", role = Role.COACH),)
+        val coachDTO = coach.toDTO()
 
-        val coach = Coach(account = account)
+        every { coachService.setCoachingTeam(team.id, principal.name) } returns coachDTO
 
-        coachService.createCoach(coach)
+        val result = teamController.addCoach(team.id, principal)
 
-        val principal = Principal { "username" }
-
-        val res = teamController.addCoach(team.id!!, principal)
-
-        assertEquals(200, res.statusCode.value())
+        assertEquals(HttpStatus.CREATED, result.statusCode)
+        assertEquals(coachDTO, result.body)
+        verify { coachService.setCoachingTeam(team.id, principal.name) }
     }
 
     @Test
-    @WithMockUser(roles = ["ADMIN"])
-    fun addInvalidCoachTest() {
-        val principal = Principal { "username" }
+    fun invitePlayerTest() {
+        val playerSummary = AccountSummary(1, "player", "player")
+        val teamSummary = team.toSummary()
+        val inviteDTO = TeamInviteDTO(teamSummary, playerSummary, InviteStatus.PENDING)
 
-        val res = teamController.addCoach(team.id!!, principal)
+        every { teamInviteService.invitePlayerByCoach(playerSummary.id, principal.name) } returns inviteDTO
 
-        assertTrue(res.statusCode.isError)
-    }
+        val result = teamController.invitePlayer(playerSummary.id, principal)
 
-    @Test
-    @WithMockUser(roles = ["ADMIN"])
-    fun inviteInvalidPlayerTest() {
-        val account = Account(
-            name = "name",
-            username = "username",
-            password = "password",
-            role = Role.ADMIN,
-        )
-
-        val coach = Coach(account = account)
-
-        coachService.createCoach(coach)
-
-        val principal = Principal { "username" }
-
-        val res = teamController.addCoach(team.id!!, principal)
-
-        assertEquals(200, res.statusCode.value())
-
-        val inviteRes = teamController.invitePlayer(0, Principal { "username" })
-
-        assertTrue(inviteRes.statusCode.isError)
-    }
-
-    @Test
-    @WithMockUser(roles = ["ADMIN"])
-    fun inviteInvalidPlayerTest1() {
-        val inviteRes = teamController.invitePlayer(0, Principal { "username" })
-
-        assertTrue(inviteRes.statusCode.isError)
-    }
-
-    @Test
-    @WithMockUser(roles = ["ADMIN"])
-    fun getMatchesTest() {
-        val response = teamController.getAll()
-
-        assertEquals(200, response.statusCode.value())
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertEquals(inviteDTO, result.body)
+        verify { teamInviteService.invitePlayerByCoach(playerSummary.id, principal.name) }
     }
 }
