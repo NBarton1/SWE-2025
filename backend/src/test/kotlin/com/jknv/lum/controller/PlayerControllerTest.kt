99c2c53848cef1afab344a8437189b726dbf8db0
nmbar <nmbarton15@gmail.com>
2025-11-07 18:46:44 -0500

    player controller tests

--- backend/src/test/kotlin/com/jknv/lum/controller/PlayerControllerTest.kt
+++ backend/src/test/kotlin/com/jknv/lum/controller/PlayerControllerTest.kt
@@ -5,9 +5,12 @@ import com.jknv.lum.model.dto.TeamInviteDTO
 import com.jknv.lum.model.entity.Account
 import com.jknv.lum.model.entity.Guardian
 import com.jknv.lum.model.entity.Player
+import com.jknv.lum.model.entity.Team
+import com.jknv.lum.model.entity.TeamInvite
 import com.jknv.lum.model.request.account.AccountCreateRequest
 import com.jknv.lum.model.request.player.PlayerInviteRequest
 import com.jknv.lum.model.request.player.PlayerPermissionUpdateRequest
+import com.jknv.lum.model.type.InviteStatus
 import com.jknv.lum.security.AccountDetails
 import com.jknv.lum.services.PlayerService
 import com.jknv.lum.services.TeamInviteService
@@ -17,92 +20,120 @@ import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
-import java.security.Principal
 import kotlin.test.assertEquals
 
 class PlayerControllerTest {
-
     val playerService: PlayerService = mockk()
     val teamInviteService: TeamInviteService = mockk()
+
+    val playerController: PlayerController = PlayerController(playerService, teamInviteService)
+
     val details: AccountDetails = mockk()
+    val guardian: Guardian = mockk()
+    val account: Account = mockk()
 
-    val controller: PlayerController = PlayerController(playerService, teamInviteService)
-
-    lateinit var req: AccountCreateRequest
-    lateinit var guardian: Guardian
     lateinit var player: Player
-    lateinit var playerDTO: PlayerDTO
+
+    val mockId = 1L
 
     @BeforeEach
     fun setup() {
-        req = AccountCreateRequest(name = "player", username = "player", password = "password")
-        guardian = Guardian(account = Account(name = "guardian", username = "guardian", password = "password"))
-        player = Player(id = 1, account = req.toEntity(), guardian = guardian)
-        playerDTO = PlayerDTO(
-            account = player.account.toDTO(),
-            guardian = guardian.account.toDTO(),
-            team = player.playingTeam?.toDTO(),
-            hasPermission = player.hasPermission,
-            position = player.position
-        )
-        every { details.id } returns guardian.account.id
+        player = Player(account = account, guardian = guardian)
+
+        every { guardian.id } returns mockId
+        every { details.id } returns guardian.id
     }
 
     @Test
     fun createPlayerTest() {
-        every { playerService.createPlayer(any(), any()) } returns playerDTO
+        val req: AccountCreateRequest = mockk()
+        val playerDTO: PlayerDTO = mockk()
 
-        val response = controller.createPlayer(req, details)
+        every { playerService.createPlayer(req, mockId) } returns playerDTO
+
+        val response = playerController.createPlayer(req, details)
+
+        verify { playerService.createPlayer(req, mockId) }
 
         assertEquals(HttpStatus.CREATED, response.statusCode)
         assertEquals(playerDTO, response.body)
-        verify { playerService.createPlayer(any(), any()) }
+    }
+
+    @Test
+    fun getPlayersTest() {
+        val playerDTO: PlayerDTO = mockk()
+
+        every { playerService.getPlayers() } returns listOf(playerDTO)
+
+        val response = playerController.getPlayers()
+
+        verify { playerService.getPlayers() }
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(listOf(playerDTO), response.body)
     }
 
     @Test
     fun setPermissionTest() {
         val req = PlayerPermissionUpdateRequest(hasPermission = true)
-        val expectedDTO = playerDTO.copy(hasPermission = true)
 
-        every { playerService.updatePlayerPermission(any(), any(), any()) } returns expectedDTO
+        val expected = PlayerDTO(
+            account = mockk(),
+            guardian = mockk(),
+            team = player.playingTeam?.toDTO(),
+            hasPermission = req.hasPermission,
+            position = player.position,
+        )
 
-        val response = controller.setPermission(player.id, req, details)
+        every { playerService.updatePlayerPermission(player.id, mockId, req.hasPermission) } returns expected
+
+        val response = playerController.setPermission(player.id, req, details)
+
+        verify { playerService.updatePlayerPermission(player.id, mockId, req.hasPermission) }
 
         assertEquals(HttpStatus.OK, response.statusCode)
-        assertEquals(expectedDTO, response.body)
-        verify { playerService.updatePlayerPermission(any(), any(), any()) }
+        assertEquals(expected, response.body)
     }
 
     @Test
     fun getInvitesTest() {
-        val invites = listOf(
-            TeamInviteDTO(
-                team = mockk(),
-                player = mockk(),
-                status = mockk()
-            )
+        every { details.id } returns player.id
+
+        val invite = TeamInviteDTO(
+            team = mockk(),
+            player = mockk(),
+            status = mockk()
         )
 
-        every { teamInviteService.getInvitesByPlayer(any()) } returns invites
+        every { teamInviteService.getInvitesByPlayer(player.id) } returns listOf(invite)
 
-        val response = controller.getInvites(details)
+        val response = playerController.getInvites(details)
+
+        verify { teamInviteService.getInvitesByPlayer(player.id) }
 
         assertEquals(HttpStatus.OK, response.statusCode)
-        assertEquals(invites, response.body)
-        verify { teamInviteService.getInvitesByPlayer(any()) }
+        assertEquals(listOf(invite), response.body)
     }
 
     @Test
     fun respondToInviteTest() {
+        every { details.id } returns player.id
+
         val req = PlayerInviteRequest(isAccepted = true)
-        val expectedDTO = playerDTO.copy(hasPermission = true)
 
-        every { teamInviteService.respondToInvite(any(), any(), any()) } returns expectedDTO
+        val expectedDTO = TeamInviteDTO(
+            team = mockk(),
+            player = mockk(),
+            status = if (req.isAccepted) InviteStatus.ACCEPTED else InviteStatus.DECLINED,
+        )
 
-        val response = controller.respondToInvite(10, req, details)
+        every { teamInviteService.respondToInvite(player.id, any(), req.isAccepted) } returns expectedDTO
+
+        val response = playerController.respondToInvite(mockId, req, details)
+
+        verify { teamInviteService.respondToInvite(player.id, mockId, req.isAccepted) }
 
         assertEquals(HttpStatus.OK, response.statusCode)
         assertEquals(expectedDTO, response.body)
-        verify { teamInviteService.respondToInvite(any(), any(), any()) }
     }
 }
