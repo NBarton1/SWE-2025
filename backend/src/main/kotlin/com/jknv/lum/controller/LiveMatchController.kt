6c43917626f7e4b1174024fb99a974c048723b02
joe <josephfielding03@gmail.com>
2025-11-07 19:37:54 -0500

    #38 admin only access to updating live match

--- backend/src/main/kotlin/com/jknv/lum/controller/LiveMatchController.kt
+++ backend/src/main/kotlin/com/jknv/lum/controller/LiveMatchController.kt
@@ -1,13 +1,19 @@
 package com.jknv.lum.controller
 
+import com.jknv.lum.LOGGER
 import com.jknv.lum.config.PreAuthorizeAdmin
 import com.jknv.lum.model.request.match.MatchUpdateRequest
+import com.jknv.lum.model.type.Role
+import com.jknv.lum.security.AccountDetails
 import com.jknv.lum.services.MatchService
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
+import java.security.Principal
 
 @Controller
 class LiveMatchController(
@@ -16,11 +22,16 @@ class LiveMatchController(
 ) {
 
     @MessageMapping("/match/live-update/{matchId}")
-    @PreAuthorizeAdmin
     fun liveUpdate(
         @DestinationVariable matchId: Long,
-        @Payload req: MatchUpdateRequest
+        @Payload req: MatchUpdateRequest,
+        principal: Principal,
     ) {
+        // TODO: workaround for now, @AuthenticationPrincipal not working
+        if (!(principal as UsernamePasswordAuthenticationToken).authorities.any { it.authority == "ROLE_ADMIN" }) {
+            throw IllegalAccessError("Only admin allowed")
+        }
+
         val updatedMatch = matchService.updateMatch(matchId, req)
         simpMessagingTemplate.convertAndSend("/topic/match/${matchId}", updatedMatch)
     }
