6a173580961a5b3cf56e6403b93954b81300af5a
joe <josephfielding03@gmail.com>
2025-11-22 15:03:23 -0500

    admin tests

--- frontend/vitest.setup.tsx
+++ frontend/vitest.setup.tsx
@@ -8,6 +8,7 @@ import {Match, type MatchResponse, MatchState, MatchType} from "./src/main/types
 import {type Account, type Coach, type Player, Role} from "./src/main/types/accountTypes.ts";
 import type {Content} from "./src/main/types/content.ts";
 import {InviteStatus, type TeamInvite} from "./src/main/types/invite.ts";
+import type {Post} from "./src/main/types/post.ts";
 
 expect.extend(matchers)
 
@@ -132,6 +133,8 @@ export const mockContent: Content = {
     downloadUrl: "",
 }
 
+export const mockUnapprovedContent: Content[] = [mockContent];
+
 export const mockPlayerAccount: Account = {
     id: 2,
     name: "Diddy Kong",
@@ -217,6 +220,20 @@ export const mockContentImage: Content = {
     downloadUrl: "http://localhost:8080/api/content/1",
 }
 
+export const mockPost: Post = {
+    id: 1,
+    account: mockAdminAccount,
+    textContent: "DK won",
+}
+
+export const mockFlaggedPost: Post = {
+    id: 2,
+    account: mockPlayerAccount,
+    textContent: "DK lost",
+    flagCount: 1000,
+}
+
+export const mockFlaggedPosts = [mockFlaggedPost]
 
 afterEach(() => {
     cleanup()
