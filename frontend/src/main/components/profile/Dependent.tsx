3648b4187c95c316f054fc0f19029652edad6258
joe <josephfielding03@gmail.com>
2025-11-22 13:11:46 -0500

    updated profile tests

--- frontend/src/main/components/profile/Dependent.tsx
+++ frontend/src/main/components/profile/Dependent.tsx
@@ -52,7 +52,6 @@ const Dependent = ({
                         data-testid={`expand-button-${dependent.account.id}`}
                     >
                         <ChevronDown
-                            data-testid="chevron"
                             size={20}
                             style={{
                                 transform: isExpanded ? "rotate(180deg)" : "rotate(0deg)",
