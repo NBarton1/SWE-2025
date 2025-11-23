1386bfcdfa2f4b1c85543f5f6b25622029f20d78
vilnisjatnieks <vjatnieks@loyola.edu>
2025-11-23 00:02:50 -0500

    success noti if all rows are valid #40

--- frontend/src/main/components/schedule/ScheduleExcelDropzone.tsx
+++ frontend/src/main/components/schedule/ScheduleExcelDropzone.tsx
@@ -1,12 +1,13 @@
 import { Dropzone } from "@mantine/dropzone";
 import { MIME_TYPES } from "@mantine/dropzone";
-import {Group, Paper, Text, Title} from "@mantine/core";
+import {Group, Paper, Stack, Text, Title} from "@mantine/core";
 import { IconUpload, IconX, IconPhoto } from "@tabler/icons-react";
 import * as XLSX from "xlsx";
 import { useState } from "react";
 import {createMatch, type CreateMatchRequest} from "../../request/matches.ts";
 import {getTeams} from "../../request/teams.ts";
 import {Match} from "../../types/match.ts";
+import { Notification } from '@mantine/core';
 
 
 const excelDateToJSDate = (excelNum: number)=> {
@@ -66,6 +67,7 @@ const getTeamDictionary = async () => {
 
 const createMatchesFromRows = async (rows: any) => {
     const teamDictionary = await getTeamDictionary();
+    let allRowsSuccesful: boolean = true;
 
     for (const row of rows) {
 
@@ -74,6 +76,7 @@ const createMatchesFromRows = async (rows: any) => {
 
         if (!homeId || !awayId) {
             console.log(`Team not found in dictionary for row:`, row);
+            allRowsSuccesful = false;
         }
         else {
             await createMatchFromRow(
@@ -88,6 +91,7 @@ const createMatchesFromRows = async (rows: any) => {
         console.log("Mapped home:", homeId, "away:", awayId);
 
     }
+    return allRowsSuccesful;
 };
 
 const createMatchFromRow = async ( type: string, homeTeamId: string, awayTeamId: string, date:string, time:string) => {
@@ -111,6 +115,8 @@ const createMatchFromRow = async ( type: string, homeTeamId: string, awayTeamId:
 
 export function ScheduleExcelImporter() {
     const [loading, setLoading] = useState(false);
+    const [showNotification, setShowNotification] = useState(false);
+
 
     const handleExcel = async (file: File) => {
         setLoading(true);
@@ -123,48 +129,62 @@ export function ScheduleExcelImporter() {
                 return;
             }
 
-            await createMatchesFromRows(rows);
+            const allRowsSuccesful: boolean = await createMatchesFromRows(rows);
+            if (allRowsSuccesful) setShowNotification(true);
+
         } finally {
             setLoading(false);
         }
     };
 
     return (
-        <Paper shadow="sm" p="md" radius="md" withBorder data-testid="create-match-form">
+        <Stack gap="md">
+            <Paper shadow="sm" p="md" radius="md" withBorder data-testid="create-match-form">
 
+                <Dropzone
+                    onDrop={(files) => handleExcel(files[0])}
+                    onReject={(files) => console.log('rejected files', files)}
+                    maxSize={5 * 1024 ** 2}
+                    accept={[MIME_TYPES.xlsx]}
+                    disabled={loading}
+                >
+                    <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+                        <Dropzone.Accept>
+                            <IconUpload size={52} color="var(--mantine-color-blue-6)" stroke={1.5} />
+                        </Dropzone.Accept>
 
+                        <Dropzone.Reject>
+                            <IconX size={52} color="var(--mantine-color-red-6)" stroke={1.5} />
+                        </Dropzone.Reject>
 
-            <Dropzone
-                onDrop={(files) => handleExcel(files[0])}
-                onReject={(files) => console.log('rejected files', files)}
-                maxSize={5 * 1024 ** 2}
-                accept={[MIME_TYPES.xlsx]}
-                disabled={loading}
-            >
-                <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-                    <Dropzone.Accept>
-                        <IconUpload size={52} color="var(--mantine-color-blue-6)" stroke={1.5} />
-                    </Dropzone.Accept>
+                        <Dropzone.Idle>
+                            <IconPhoto size={52} color="var(--mantine-color-dimmed)" stroke={1.5} />
+                        </Dropzone.Idle>
 
-                    <Dropzone.Reject>
-                        <IconX size={52} color="var(--mantine-color-red-6)" stroke={1.5} />
-                    </Dropzone.Reject>
+                        <div>
+                            <Text size="xl" inline>
+                                Drag your file here or click to select file
+                            </Text>
+                            <Text size="sm" c="dimmed" inline mt={7}>
+                                File must be an .xlsx file and under 5mb
+                            </Text>
+                        </div>
+                    </Group>
+                </Dropzone>
 
-                    <Dropzone.Idle>
-                        <IconPhoto size={52} color="var(--mantine-color-dimmed)" stroke={1.5} />
-                    </Dropzone.Idle>
+            </Paper>
 
-                    <div>
-                        <Text size="xl" inline>
-                            Drag your file here or click to select file
-                        </Text>
-                        <Text size="sm" c="dimmed" inline mt={7}>
-                            File must be an .xlsx file and under 5mb
-                        </Text>
-                    </div>
-                </Group>
-            </Dropzone>
-        </Paper>
+            <Text size="sm">Note: Excel headers must be type, homeTeam, awayTeam, date, time</Text>
+
+            {showNotification && (
+                <Notification color="green"
+                              title="Successfully imported schedule!"
+                              onClose={() => setShowNotification(false)}>
+                    Close to view the changes.
+                </Notification>
+            )}
+
+        </Stack>
 
     );
 }
